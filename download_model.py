6db21ca82dd570cc3f1b6afb796ee393863d2588
TrentBrick <tbb21@duke.edu>
2019-07-31 02:21:20 +0000

    first push, going to remove all the f strings

--- download_model.py
+++ download_model.py
@@ -8,19 +8,19 @@ parser.add_argument(
     type=str,
     help='Valid model names: (base|large)',
 )
-model_type = parser.parse_args().model_type
+model_type = 'large'#parser.parse_args().model_type
 
 model_dir = os.path.join('models', model_type)
 if not os.path.exists(model_dir):
     os.makedirs(model_dir)
 
 for ext in ['data-00000-of-00001', 'index', 'meta']:
-    r = requests.get(f'https://storage.googleapis.com/grover-models/{model_type}/model.ckpt.{ext}', stream=True)
-    with open(os.path.join(model_dir, f'model.ckpt.{ext}'), 'wb') as f:
+    r = requests.get('https://storage.googleapis.com/grover-models/'+model_type+'/model.ckpt.'+ext, stream=True)
+    with open(os.path.join(model_dir, 'model.ckpt.'+ext), 'wb') as f:
         file_size = int(r.headers["content-length"])
         if file_size < 1000:
             raise ValueError("File doesn't exist? idk")
         chunk_size = 1000
         for chunk in r.iter_content(chunk_size=chunk_size):
             f.write(chunk)
-    print(f"Just downloaded {model_type}/model.ckpt.{ext}!", flush=True)
+    print("Just downloaded {model_type}/model.ckpt.!"+ext, flush=True)
